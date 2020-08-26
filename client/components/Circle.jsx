14eab2f46d3a47c2fc37e5131ff43e297b026c86
aidan-starke <starke.aidan@gmail.com>
2020-08-27 11:57:07 +1200

    render circles

---
+++ client/components/Circle.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import { getColor } from '../color'
+
+export default class Circle extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            circles: []
+          }
+    }
+
+    handleMouseOver = () => {
+        const circles = []
+        const { cx, cy, r } = this.props.circle
+
+        circles.push(
+            { cx: cx, cy: cy - r, r: r / 2 },
+            { cx: cx, cy: cy + r, r: r / 2 },
+            { cx: cx + r, cy: cy, r: r / 2},
+            { cx: cx - r, cy: cy, r: r / 2}
+        )
+
+        this.setState({ circles })
+    }
+
+    render() {
+        const { cx, cy, r } = this.props.circle
+        const randomNumber = Math.floor(Math.random() * 10000)
+        const color = getColor(randomNumber)
+        return (
+            <g>
+            <circle cx={cx} cy={cy} r={r} fill={color} onMouseOver={this.handleMouseOver} />
+            {this.state.circles.map((circle, key) => {
+              return <Circle key={key} circle={circle} />
+            })}
+            </g>
+        )
+    }
+}
