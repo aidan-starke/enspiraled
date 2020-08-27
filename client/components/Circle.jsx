7feb9ebadf2a61242b02266ff4c45c094499c83b
aidan-starke <starke.aidan@gmail.com>
2020-08-27 12:07:48 +1200

    refactor

--- client/components/Circle.jsx
+++ client/components/Circle.jsx
@@ -27,13 +27,14 @@ export default class Circle extends React.Component {
 
     render() {
         const { cx, cy, r } = this.props.circle
+        const { circles } = this.state
         const randomNumber = Math.floor(Math.random() * 10000)
         const color = getColor(randomNumber)
         
         return (
             <g>
             <circle cx={cx} cy={cy} r={r} fill={color} onMouseOver={this.handleMouseOver} />
-            {this.state.circles.map((circle, key) => <Circle key={key} circle={circle} />)}
+            {circles.map((circle, key) => <Circle key={key} circle={circle} />)}
             </g>
         )
     }
